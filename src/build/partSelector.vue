cae49c04bed1314858b72b856500a2ea5bd6721e
llvllohd <llvllohd@gmail.com>
2020-10-21 10:02:27 +0530

    updated robot

--- src/build/partSelector.vue
+++ src/build/partSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="['part', position]">
-    <img :src="selectedPart.src" title="arm" />
+    <img @click="showPartInfo()" :src="selectedPart.src" title="arm" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -44,6 +44,12 @@ export default {
     this.emitSelectedPart();
   },
   methods: {
+    showPartInfo() {
+      this.$router.push({
+        name: "Parts",
+        params: { partType: this.selectedPart.type, id: this.selectedPart.id },
+      });
+    },
     emitSelectedPart() {
       this.$emit("partSelected", this.selectedPart);
     },
---
+++ src/parts/PartsInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ part.title }}</h1>
+    <div>
+      {{ part.description }}
+    </div>
+  </div>
+</template>
+
+<script>
+import parts from "../data/parts";
+export default {
+  name: "PartsInfo",
+  computed: {
+    part() {
+      const { partType, id } = this.$route.params;
+      // const partType = this.$route.params.partType;
+      // const id = this.$route.params.id;
+      return parts[partType].find((part) => part.id === +id);
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
